f643283b1e9554a9111900fb65c941640f9dd9b6
Manuel Cota <56351434+mannynacc@users.noreply.github.com>
2022-03-31 11:21:32 -0600

    Added Material UI components
    Added Material UI Core and icons

--- LERT/frontend/src/App.js
+++ LERT/frontend/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   const [getMessage, setGetMessage] = useState({})
