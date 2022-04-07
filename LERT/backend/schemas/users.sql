44180687de12fd3ef0c463465516ade60c41856a
Nacho Cota <manuelcota01@outlook.com>
2022-04-06 23:45:06 -0500

    Fixed misspelling in test schema

--- LERT/backend/schemas/users.sql
+++ LERT/backend/schemas/users.sql
@@ -3,14 +3,14 @@ CREATE TABLE users
     id SERIAL PRIMARY KEY,
     full_name VARCHAR ( 100 ) NOT NULL,
     email VARCHAR (100) NOT NULL,
-    passwd VARCHAR (100) NOT NULL
+    passwd VARCHAR (300) NOT NULL
 );
 
 /*Temporary values*/
 INSERT into users (full_name, email, passwd) 
 VALUES ('Remy Sharp', 
 		'remysharp@ibm.com', 
-		'ha256:260000$zaUZiqIsUom0M1Cq$41505f9ddf53fda2a5cf99ea6f569cb15577a40e574df494370189f39b99ef97');
+		'pbkdf2:sha256:260000$zaUZiqIsUom0M1Cq$41505f9ddf53fda2a5cf99ea6f569cb15577a40e574df494370189f39b99ef97');
 
 /*Verify that dev account was created*/
 SELECT * FROM users;
