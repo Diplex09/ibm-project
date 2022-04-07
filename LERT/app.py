bd657dee6b0d3f2fbd2a65f76c1d320dd4678e50
Diego Velázquez Moreno <diegoa1962@hotmail.com>
2022-04-06 21:24:09 -0500

    Backend login & logout

--- LERT/app.py
+++ LERT/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify, request, session, send_from_directory
 from flask_restful import Api, Resource, reqparse
+#library for encrypting
+from werkzeug.security import generate_password_hash, check_password_hash
+#from flask_cors import CORS #pip install -U flask-cors
+from datetime import timedelta
+import psycopg2
+import psycopg2.extras
+
 from flask_cors import CORS #comment this on deployment
 from backend.HelloApiHandler import HelloApiHandler
+from backend.login import login
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
@@ -11,4 +19,75 @@ api = Api(app)
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
+
+app.config['SECRET_KEY'] = 'cairocoders-ednalan'
+
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
+CORS(app)
+
+DB_HOST = "localhost"
+DB_NAME = "sampledb"
+DB_USER = "postgres"
+DB_PASS = "jodial"
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+
+
+#/login 
+@app.route("/")
+def home():
+        passhash = generate_password_hash('cairocoders')
+        print(passhash)
+        if 'username' in session:
+            username = session['username']
+            return jsonify({'message' : 'You are already logged in','username': username})
+        else:
+            resp = jsonify({'message' : 'Unauthorized'})
+            resp.status_code = 401
+            return resp
+
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    _json = request.json
+    _username = _json['username']
+    _password = _json['password']
+    print(_password)
+    #validate the received values
+    if _username and _password:
+        #check user exists
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        sql = "SELECT * FROM useraccount WHERE username=%s"
+        sql_where = (_username,)
+
+        cursor.execute(sql, sql_where)
+        row = cursor.fetchone() 
+        username = row['username']
+        password = row['password']
+        if row:
+            if check_password_hash(password, _password):
+                session['username'] = username
+                cursor.close()
+                return jsonify ({'message' : 'You are logged in successfully'})
+            else:
+                resp = jsonify({'message' : 'Bad Request - invalid password'})
+                resp.status_code = 400
+                return resp
+
+
+    else:
+        resp = jsonify({'message' : 'Bad Request - invalid credentials'})
+        resp.status_code = 400
+        return resp
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session.pop('username', None)
+    return jsonify({'message':'You successfully logged out'})
+
 api.add_resource(HelloApiHandler, '/flask/hello')
+
+#api.add_resource(login, '/flask/login')
