6ef625062d1debd3c09264acaaf7b9f4f3b7ce3f
Nacho Cota <manuelcota01@outlook.com>
2022-04-06 21:53:06 -0500

    Added test data and instructions

--- LERT/backend/schemas/users.sql
+++ LERT/backend/schemas/users.sql
@@ -1,6 +1,16 @@
 CREATE TABLE users
 (
-    id serial PRIMARY KEY,
-    username VARCHAR (100) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR ( 100 ) NOT NULL,
+    email VARCHAR (100) NOT NULL,
     passwd VARCHAR (100) NOT NULL
 );
+
+/*Temporary values*/
+INSERT into users (full_name, email, passwd) 
+VALUES ('Remy Sharp', 
+		'remysharp@ibm.com', 
+		'ha256:260000$zaUZiqIsUom0M1Cq$41505f9ddf53fda2a5cf99ea6f569cb15577a40e574df494370189f39b99ef97');
+
+/*Verify that dev account was created*/
+SELECT * FROM users;
