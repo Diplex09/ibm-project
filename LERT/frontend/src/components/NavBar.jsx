32901df7dd5decf5763c57f80866fb6053312932
Alonso G <71342528+AlonsoMtzG@users.noreply.github.com>
2022-04-08 01:02:46 -0500

    Update NavBar variant open/close

--- LERT/frontend/src/components/NavBar.jsx
+++ LERT/frontend/src/components/NavBar.jsx
@@ -1,114 +1,160 @@
+import { useState } from "react";
+import { styled, useTheme } from "@mui/material/styles";
 import {
-    Avatar,
-    Drawer,
-    Toolbar,
+    Box,
     List,
-    Typography,
+    CssBaseline,
     Divider,
-    ListItemText,
+    IconButton,
     ListItemButton,
     ListItemIcon,
+    ListItemText,
 } from "@mui/material";
-
+import MuiDrawer from "@mui/material/Drawer";
 import {
-    HomeOutlined,
-    GroupAddOutlined,
     AccountBalanceOutlined,
+    GroupAddOutlined,
+    HomeOutlined,
+    LogoutOutlined,
     LoopOutlined,
-    PostAddOutlined,
     PersonOutlineOutlined,
+    PostAddOutlined,
+    Menu,
+    ChevronRight,
 } from "@mui/icons-material";
 
+const drawerWidth = 300;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up("sm")]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, {
+    shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+    boxSizing: "border-box",
+    ...(open && {
+        ...openedMixin(theme),
+        "& .MuiDrawer-paper": openedMixin(theme),
+    }),
+    ...(!open && {
+        ...closedMixin(theme),
+        "& .MuiDrawer-paper": closedMixin(theme),
+    }),
+}));
+
 export const NavBar = () => {
+    const theme = useTheme();
+
+    const [open, setOpen] = useState(false);
+
+    const handleDrawerClose = () => {
+        if (open === true) {
+            setOpen(false);
+        } else {
+            setOpen(true);
+        }
+    };
+
     return (
-        <Drawer
-            sx={{
-                width: 300,
-                flexShrink: 0,
-                "& .MuiDrawer-paper": {
-                    width: 300,
-                    boxSizing: "border-box",
-                },
-            }}
-            variant="permanent"
-            anchor="right"
-        >
-            <Toolbar sx={{ marginTop: 2, marginBottom: 2 }}>
-                <List sx={{ margin: "auto" }}>
-                    <Avatar
-                        alt="Remy Sharp"
-                        src="/static/images/avatar/1.jpg"
-                        sx={{
-                            width: 56,
-                            height: 56,
-                            margin: "auto",
-                            marginBottom: 2,
-                        }}
-                    />
-                    <h3>Sebastián Rojas</h3>
-                    <Typography
-                        sx={{
-                            fontSize: ".7rem",
-                            marginBottom: 1,
-                            textAlign: "center",
-                        }}
-                    >
-                        Administrador
-                    </Typography>
+        <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <Drawer variant="permanent" anchor="right" open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {open === true ? (
+                            <ChevronRight />
+                        ) : (
+                            <Menu sx={{ color: "#0062ff" }} />
+                        )}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <List>
+                    {[
+                        {
+                            text: "Home",
+                            icon: <HomeOutlined />,
+                        },
+                        {
+                            text: "Delegate",
+                            icon: <GroupAddOutlined />,
+                        },
+                        {
+                            text: "Employee",
+                            icon: <PersonOutlineOutlined />,
+                        },
+                        {
+                            text: "Expenses",
+                            icon: <AccountBalanceOutlined />,
+                        },
+                        {
+                            text: "Recovery",
+                            icon: <LoopOutlined />,
+                        },
+                        {
+                            text: "Reports",
+                            icon: <PostAddOutlined />,
+                        },
+                        {
+                            text: "Logout",
+                            icon: <LogoutOutlined />,
+                        },
+                    ].map(({ text, icon }, index) => (
+                        <ListItemButton
+                            key={text}
+                            sx={{
+                                minHeight: 48,
+                                justifyContent: open ? "initial" : "center",
+                                px: 2.5,
+                            }}
+                        >
+                            <ListItemIcon
+                                sx={{
+                                    minWidth: 0,
+                                    mr: open ? 3 : "auto",
+                                    justifyContent: "center",
+                                }}
+                            >
+                                {icon}
+                            </ListItemIcon>
+                            <ListItemText
+                                primary={text}
+                                sx={{ opacity: open ? 1 : 0 }}
+                            />
+                        </ListItemButton>
+                    ))}
                 </List>
-            </Toolbar>
-            <Divider />
-            <List>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <HomeOutlined />
-                    </ListItemIcon>
-                    <ListItemText primary="Home" />
-                </ListItemButton>
-
-                <ListItemButton>
-                    <ListItemIcon>
-                        <GroupAddOutlined />
-                    </ListItemIcon>
-                    <ListItemText primary="Delegate" />
-                </ListItemButton>
-
-                <ListItemButton>
-                    <ListItemIcon>
-                        <PersonOutlineOutlined />
-                    </ListItemIcon>
-                    <ListItemText primary="Employee" />
-                </ListItemButton>
-
-                <ListItemButton>
-                    <ListItemIcon>
-                        <AccountBalanceOutlined />
-                    </ListItemIcon>
-                    <ListItemText primary="Expenses" />
-                </ListItemButton>
-
-                <ListItemButton>
-                    <ListItemIcon>
-                        <LoopOutlined />
-                    </ListItemIcon>
-                    <ListItemText primary="Recovery" />
-                </ListItemButton>
-
-                <ListItemButton>
-                    <ListItemIcon>
-                        <PostAddOutlined />
-                    </ListItemIcon>
-                    <ListItemText primary="Reports" />
-                </ListItemButton>
-
-                <ListItemButton>
-                    <ListItemIcon>
-                        <PersonOutlineOutlined />
-                    </ListItemIcon>
-                    <ListItemText primary="Logout" />
-                </ListItemButton>
-            </List>
-            <Divider />
-        </Drawer>
+            </Drawer>
+        </Box>
     );
 };
