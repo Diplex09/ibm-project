f8cd75e81edacbd2b90118aaa449190ecbd6f942
Diego Velázquez Moreno <diegoa1962@hotmail.com>
2022-04-20 17:37:34 -0500

    Update Users table
    I fix how the roles were made  and add the rol

--- LERT/backend/schemas/users.sql
+++ LERT/backend/schemas/users.sql
@@ -1,19 +1,22 @@
 /*NOTE: Run this command if you haven't created DB in pgadmin4 before*/
 CREATE DATABASE lert;
 
-CREATE TABLE users
+CREATE TABLE Users 
 (
-    id SERIAL PRIMARY KEY,
-    full_name VARCHAR ( 100 ) NOT NULL,
-    email VARCHAR (100) NOT NULL,
-    passwd VARCHAR (300) NOT NULL
+	Id_user SERIAL PRIMARY KEY ,
+	FullName varchar (100) NOT NULL,
+	Mail varchar(100) NOT NULL,
+	Passwd varchar (300) NOT NULL,
+	Rol int
 );
 
+
 /*Temporary values*/
-INSERT into users (full_name, email, passwd) 
+INSERT into users (FullName, Mail, Passwd, Rol) 
 VALUES ('Remy Sharp', 
 		'remysharp@ibm.com', 
-		'pbkdf2:sha256:260000$zaUZiqIsUom0M1Cq$41505f9ddf53fda2a5cf99ea6f569cb15577a40e574df494370189f39b99ef97');
-
+		'pbkdf2:sha256:260000$zaUZiqIsUom0M1Cq$41505f9ddf53fda2a5cf99ea6f569cb15577a40e574df494370189f39b99ef97', 
+		'1');
+        
 /*Verify that dev account was created*/
 SELECT * FROM users;
