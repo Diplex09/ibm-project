3c635683e102b7cd57a91d5594f83ed5db9910f8
Manuel Cota <56351434+mannynacc@users.noreply.github.com>
2022-04-21 09:40:49 -0500

    Added Rol foreign key to Users

--- LERT/backend/schemas/users.sql
+++ LERT/backend/schemas/users.sql
@@ -1,22 +1,27 @@
 /*NOTE: Run this command if you haven't created DB in pgadmin4 before*/
 CREATE DATABASE lert;
 
+/*NOTE: Create ROLES table BEFORE Users*/
+
 CREATE TABLE Users 
 (
 	Id_user SERIAL PRIMARY KEY ,
 	FullName varchar (100) NOT NULL,
 	Mail varchar(100) NOT NULL,
 	Passwd varchar (300) NOT NULL,
-	Rol int
+	Rol int,
+	CONSTRAINT user_rol
+      		FOREIGN KEY(Rol) 
+	  			REFERENCES Roles(id)
 );
 
 
 /*Temporary values*/
-INSERT into users (FullName, Mail, Passwd, Rol) 
+INSERT into Users (FullName, Mail, Passwd, Rol) 
 VALUES ('Remy Sharp', 
 		'remysharp@ibm.com', 
 		'pbkdf2:sha256:260000$zaUZiqIsUom0M1Cq$41505f9ddf53fda2a5cf99ea6f569cb15577a40e574df494370189f39b99ef97', 
-		'1');
+		1);
         
 /*Verify that dev account was created*/
 SELECT * FROM users;
