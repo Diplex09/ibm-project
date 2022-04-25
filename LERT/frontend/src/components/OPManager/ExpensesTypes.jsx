400caad19bcf463037d932c1364b4e8bb2511351
Sreddx <rssebas08@outlook.com>
2022-04-25 17:45:44 -0500

    Created Expenses types table with json
    Working on backend connection

--- LERT/frontend/src/components/OPManager/ExpensesTypes.jsx
+++ LERT/frontend/src/components/OPManager/ExpensesTypes.jsx
@@ -1,83 +1,149 @@
-import { List, Typography, Button, Input, Grid, Box, TextField, Paper } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+
+import {
+    List,
+    Typography,
+    Button,
+    Input,
+    Grid,
+    Box,
+    TextField,
+    Paper,
+    DataTable,
+} from "@mui/material";
 import React, { useState } from "react";
+import data1 from "./dataTest";
 
 export const ExpenseForm = () => {
     const [textValue, setTextValue] = useState("");
-  
+
     const onTextChange = (e) => setTextValue(e.target.value);
     const handleSubmit = () => console.log(textValue);
     const handleReset = () => setTextValue("");
-  
-    return (
-      
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-        <TextField
-          fullWidth
-          onChange={onTextChange}
-          value={textValue}
-          label={"Enter expense type"} //optional
-        />
-        </Grid>
-        <Grid item xs={4}>
-          <Button onClick={handleSubmit}
-          variant="contained"
-          sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              textTransform: "none",
-              borderRadius: "0px",
-              width: "12rem",
-              height: "40px",
-              fontSize: "15px",
-              fontWeight: "400",
-              bgcolor: "#0062ff",
-              ":hover": {
-                  bgcolor: "#0255DA",
-              },
-          }}
-          
-          >Submit</Button>
-          <Button onClick={handleReset}
-          variant="contained"
-          sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              textTransform: "none",
-              borderRadius: "0px",
-              width: "12rem",
-              height: "40px",
-              fontSize: "15px",
-              fontWeight: "400",
-              bgcolor: "#0062ff",
-              ":hover": {
-                  bgcolor: "#0255DA",
-              },
-          }}
-          >Reset</Button>
-        </Grid>
-      </Grid>
-      
-    );
-  };
 
-export const ExpensesTypes = () =>{
     return (
-    <Box
-    sx={{
-        width: "100%",
-        height: "100%"
-    }}
-    >
-        <Typography
-                align = 'center'
+        <Grid container spacing={2}>
+            <Grid item xs={10}>
+                <TextField
+                    fullWidth
+                    onChange={onTextChange}
+                    value={textValue}
+                    label={"Enter expense type"} //optional
+                />
+            </Grid>
+            <Grid item xs={2}>
+                <Button
+                    onClick={handleSubmit}
+                    variant="contained"
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        textTransform: "none",
+                        borderRadius: "0px",
+                        width: "12rem",
+                        height: "40px",
+                        fontSize: "15px",
+                        fontWeight: "400",
+                        bgcolor: "#0062ff",
+                        ":hover": {
+                            bgcolor: "#0255DA",
+                        },
+                    }}
+                >
+                    Submit
+                </Button>
+                {/* <Button
+                    onClick={handleReset}
+                    variant="contained"
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        textTransform: "none",
+                        borderRadius: "0px",
+                        width: "12rem",
+                        height: "40px",
+                        fontSize: "15px",
+                        fontWeight: "400",
+                        bgcolor: "#0062ff",
+                        ":hover": {
+                            bgcolor: "#0255DA",
+                        },
+                    }}
+                >
+                    Reset
+                </Button> */}
+            </Grid>
+        </Grid>
+    );
+};
+
+export const ExpensesTable = () => {
+    return (
+        <TableContainer
+            component={Paper}
+            sx={{
+                "& .MuiTableCell-head": {
+                    color: "#0062ff",
+                    textTransform: "uppercase",
+                    fontWeight: "500",
+                },
+                padding: "5px 20px",
+            }}
+        >
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell align="left">Expense Type Name</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data1.map((row) => (
+                        <TableRow
+                            key={row.id}
+                            sx={{
+                                "&:last-child td, &:last-child th": {
+                                    border: 0,
+                                },
+                            }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.id}
+                            </TableCell>
+                            <TableCell align="left">
+                                {row.expenseTypeName}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export const ExpensesTypes = () => {
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                height: "100%",
+            }}
+        >
+            <Typography
+                align="center"
                 variant="h3"
                 sx={{ marginBottom: "4.5rem", fontWeight: "300" }}
             >
-                New expense type
-        </Typography>
-      
-        <ExpenseForm/>
-    </Box>
+                New Type of Expense
+            </Typography>
+
+            <ExpenseForm />
+            <ExpensesTable />
+        </Box>
     );
-}
+};
---
+++ LERT/frontend/src/components/OPManager/dataTest.js
@@ -0,0 +1,24 @@
+const data1 = [
+    {
+        id: 1,
+        expenseTypeName: "Viaje",
+    },
+    {
+        id: 2,
+        expenseTypeName: "Curso",
+    },
+    {
+        id: 3,
+        expenseTypeName: "Comida",
+    },
+    {
+        id: 4,
+        expenseTypeName: "Computadora",
+    },
+    {
+        id: 5,
+        expenseTypeName: "Pantalla",
+    },
+];
+
+export default data1;
