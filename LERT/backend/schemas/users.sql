60f7c55d74e983f50550ac1b2661f8f67753b72d
Alonso G <71342528+AlonsoMtzG@users.noreply.github.com>
2022-04-27 15:49:13 -0500

    Insert Statment for OPManager & Admin User

--- LERT/backend/schemas/users.sql
+++ LERT/backend/schemas/users.sql
@@ -17,11 +17,26 @@ CREATE TABLE Users
 
 
 /*Temporary values*/
+/* Manager */
 INSERT into Users (FullName, Mail, Passwd, Rol) 
 VALUES ('Remy Sharp', 
 		'remysharp@ibm.com', 
 		'pbkdf2:sha256:260000$zaUZiqIsUom0M1Cq$41505f9ddf53fda2a5cf99ea6f569cb15577a40e574df494370189f39b99ef97', 
 		1);
-        
+
+/* OP Manager User */
+INSERT into Users (FullName, Mail, Passwd, Rol) 
+VALUES ('Test OPManager', 
+		'testopmanager@ibm.com', 
+		'pbkdf2:sha256:260000$zaUZiqIsUom0M1Cq$41505f9ddf53fda2a5cf99ea6f569cb15577a40e574df494370189f39b99ef97', 
+		2);
+
+		/* Admin User */
+INSERT into Users (FullName, Mail, Passwd, Rol) 
+VALUES ('Test Admin', 
+		'testadmin@ibm.com', 
+		'pbkdf2:sha256:260000$zaUZiqIsUom0M1Cq$41505f9ddf53fda2a5cf99ea6f569cb15577a40e574df494370189f39b99ef97', 
+		3);
+
 /*Verify that dev account was created*/
 SELECT * FROM users;
