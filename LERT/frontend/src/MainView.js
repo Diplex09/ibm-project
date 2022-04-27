8a58de5c0b58c43658e0120f43a01c571aca6043
Alonso G <71342528+AlonsoMtzG@users.noreply.github.com>
2022-04-27 15:54:50 -0500

    Get rol from store in MainView
    - To send it as props to NavBar component

--- LERT/frontend/src/MainView.js
+++ LERT/frontend/src/MainView.js
@@ -1,9 +1,13 @@
 import { Box, CssBaseline } from '@mui/material';
+import { useSelector } from 'react-redux';
+
 import { NavBar } from './components/NavBar';
 import { Header } from './components/Header';
 import { MainContent } from './components/MainContent';
 
 export default function MainView() {
+    const { rol } = useSelector((state) => state.auth);
+
     return (
         <Box
             sx={{
@@ -15,7 +19,7 @@ export default function MainView() {
         >
             <CssBaseline />
             <Header />
-            <NavBar userRole={3} />
+            <NavBar userRole={rol} />
             <MainContent />
         </Box>
     );
