65931fac8438c793cf722ae5f6e3ebfc7c2fe0ea
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2022-04-27 15:16:04 -0500

    Design Updated

--- LERT/frontend/src/components/Manager/QuarterMg.jsx
+++ LERT/frontend/src/components/Manager/QuarterMg.jsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import Paper from "@mui/material/Paper";
 import {
     Chart,
     BarSeries,
     Title,
     ArgumentAxis,
     ValueAxis,
+<<<<<<< Updated upstream
 } from '@devexpress/dx-react-chart-material-ui';
 import { Animation } from '@devexpress/dx-react-chart';
 
@@ -14,6 +15,17 @@ const data = [
     { year: 'Q2', population: 3018 },
     { year: 'Q3', population: 5682 },
     { year: 'Q4', population: 7440 },
+=======
+} from "@devexpress/dx-react-chart-material-ui";
+import { Animation } from "@devexpress/dx-react-chart";
+import { Typography, Box } from "@mui/material";
+
+const data = [
+    { quarter: "Q1", cost: 2500 },
+    { quarter: "Q2", cost: 3018 },
+    { quarter: "Q3", cost: 5682 },
+    { quarter: "Q4", cost: 7440 },
+>>>>>>> Stashed changes
 ];
 
 export class QuarterMg extends React.PureComponent {
@@ -29,6 +41,7 @@ export class QuarterMg extends React.PureComponent {
         const { data: chartData } = this.state;
 
         return (
+<<<<<<< Updated upstream
             <Paper>
                 <Chart data={chartData}>
                     <ArgumentAxis />
@@ -40,6 +53,55 @@ export class QuarterMg extends React.PureComponent {
                     <Animation />
                 </Chart>
             </Paper>
+=======
+            <Box sx={{ background: "white" }}>
+                <Typography
+                    component="h1"
+                    variant="h3"
+                    sx={{
+                        marginLeft: 10,
+                        paddingTop: 3,
+                        paddingBottom: 3,
+                        fontWeight: "Bold",
+                    }}
+                >
+                    Table per Quarter
+                </Typography>
+                <Box
+                    sx={{
+                        width: 1050,
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        background: "white",
+                    }}
+                >
+                    <Chart data={chartData}>
+                        <ArgumentAxis />
+                        <ValueAxis max={9000} />
+
+                        <BarSeries
+                            color="#0F62FE"
+                            valueField="cost"
+                            argumentField="quarter"
+                        />
+                        <Animation />
+                    </Chart>
+
+                    <Typography
+                        component="h1"
+                        variant="h5"
+                        sx={{
+                            paddingTop: 3,
+                            paddingBottom: 3,
+                            fontWeight: "Bold",
+                            textAlign: "center",
+                        }}
+                    >
+                        Quarters
+                    </Typography>
+                </Box>
+            </Box>
+>>>>>>> Stashed changes
         );
     }
 }
