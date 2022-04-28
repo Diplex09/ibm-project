261f39aad0a9635465094640c6a053172285928b
Alonso G <71342528+AlonsoMtzG@users.noreply.github.com>
2022-04-27 20:15:51 -0500

    Log separated by components (& Design Update)
    - Added Left.jsx and Right.jsx components
    - Complete style update

--- LERT/frontend/src/components/Login.jsx
+++ LERT/frontend/src/components/Login.jsx
@@ -1,34 +1,9 @@
-import {
-    Box,
-    CssBaseline,
-    Container,
-    Avatar,
-    Typography,
-    TextField,
-    FormControlLabel,
-    Checkbox,
-    Button,
-    Grid,
-    Link,
-    CardMedia,
-} from '@mui/material';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { Box, CssBaseline, Container } from '@mui/material';
 
-import { startLogin } from '../actions/auth';
+import { Left } from './Login/Left';
+import { Right } from './Login/Right';
 
 const Login = () => {
-    const dispatch = useDispatch();
-
-    const [user, setUser] = useState('');
-    const [password, setPassword] = useState('');
-
-    const validLogin = (e) => {
-        e.preventDefault();
-        dispatch(startLogin(user, password));
-    };
-
     return (
         <Container component="main" maxWidth="false">
             <CssBaseline />
@@ -39,7 +14,7 @@ const Login = () => {
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
-                    backgroundColor: '#f2f4f8',
+                    backgroundColor: 'var(--cool-gray10)',
                 }}
             >
                 <Box
@@ -48,143 +23,10 @@ const Login = () => {
                         height: '70%',
                         display: 'flex',
                         alignItems: 'center',
-                        boxShadow: '0 10px 50px #9a9a9a',
                     }}
                 >
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            width: '50%',
-                            height: '100%',
-                            backgroundColor: '#ffffff',
-                        }}
-                    >
-                        <Avatar sx={{ m: 1, bgcolor: '#000D4B' }}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Sign in
-                        </Typography>
-
-                        <Box
-                            component="form"
-                            noValidate
-                            sx={{ mt: 1, width: '70%' }}
-                        >
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                                onChange={(e) => {
-                                    setUser(e.target.value);
-                                }}
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                onChange={(e) => {
-                                    setPassword(e.target.value);
-                                }}
-                                autoComplete="current-password"
-                            />
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        value="remember"
-                                        color="primary"
-                                    />
-                                }
-                                label="Remember me"
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                onClick={validLogin}
-                                sx={{
-                                    mt: 3,
-                                    mb: 2,
-                                    borderRadius: '0px',
-                                    bgcolor: '#0062ff',
-                                    ':hover': {
-                                        bgcolor: '#0255DA',
-                                    },
-                                }}
-                            >
-                                Sign In
-                            </Button>
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        Forgot password?
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                    </Box>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            width: '50%',
-                            height: '100%',
-                            backgroundColor: '#000D4B',
-                            color: '#ffffff',
-                        }}
-                    >
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                            }}
-                        >
-                            <Box sx={{ width: '110px', marginRight: '20px' }}>
-                                <CardMedia
-                                    component="img"
-                                    sx={{
-                                        width: 160,
-                                    }}
-                                    image="/assets/ibmBW.png"
-                                    alt="IBM"
-                                />
-                            </Box>
-
-                            <Typography
-                                variant="h3"
-                                sx={{ fontWeight: 'bold' }}
-                            >
-                                LERT
-                            </Typography>
-                        </Box>
-                        <Box
-                            sx={{
-                                width: '80%',
-                                height: '2px',
-                                marginTop: '20px',
-                                marginBottom: '20px',
-                                backgroundColor: '#ffffff',
-                            }}
-                        ></Box>
-                        <Typography component="p">
-                            Labor Expenses Recovery Tool
-                        </Typography>
-                    </Box>
+                    <Left />
+                    <Right />
                 </Box>
             </Box>
         </Container>
