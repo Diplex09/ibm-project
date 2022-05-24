0cd30fd48600f8cc39c90f4463c04e4729935580
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2022-05-23 19:40:30 -0500

    schemas updated

--- LERT/backend/schemas/type.sql
+++ LERT/backend/schemas/type.sql
@@ -2,9 +2,9 @@ CREATE TABLE Type
 (
 	Id_Type SERIAL PRIMARY KEY,
 	Type_Name Varchar (200),
-	Band Varchar (50),
+	Band integer,
 	Country Varchar (120),
-	Rate FLOAT,
-	Date_To_Start Varchar (100),
-	Date_To_Finish Varchar (100)
+	Rate integer,
+	Date_To_Start DATE,
+	Date_To_Finish DATE
 );
