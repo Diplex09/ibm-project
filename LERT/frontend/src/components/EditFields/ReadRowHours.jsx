51eca68531c0a9038843b534a5e2bc9917ed53b3
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2022-05-24 11:51:28 -0500

    Delete hour

--- LERT/frontend/src/components/EditFields/ReadRowHours.jsx
+++ LERT/frontend/src/components/EditFields/ReadRowHours.jsx
@@ -10,7 +10,7 @@ export const ReadRowHours = ({ row, handleEditClick, deleteRecord }) => {
                     <ModeEditOutlineOutlined />
                 </IconButton>
 
-                <IconButton>
+                <IconButton onClick={(e) => deleteRecord(e, row)}>
                     <DeleteOutlined />
                 </IconButton>
             </TableCell>
