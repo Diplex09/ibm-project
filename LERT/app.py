057aa8c4e5dca9ed0b2dfe5b7440174d498a2b67
Alonso G <71342528+AlonsoMtzG@users.noreply.github.com>
2022-05-24 22:18:29 -0500

    Changed endpoints protection
    Instead of check "'username' in session" now the JWT is required with "@jwt_required()"

--- LERT/app.py
+++ LERT/app.py
@@ -83,18 +83,11 @@ def refresh_expiring_jwts(response):
 
 #/login 
 @app.route("/")
+@jwt_required()
 def home():
-        # passhash = generate_password_hash('password')
-        # print(passhash)
-        if 'username' in session:
-            username = session['username']
-            return jsonify({'message' : 'You are already logged in','username': username})
-        else:
-            resp = jsonify({'message' : 'Unauthorized'})
-            resp.status_code = 401
-            return resp
-
-        # return passhash
+    identity=get_jwt_identity()
+    return jsonify({'message' : 'You are already logged in','uid': identity})
+       
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -149,9 +142,6 @@ def login():
 
 @app.route('/logout')
 def logout():
-    if 'username' in session:
-        session.pop('username', None)
-
     response = jsonify({'msg':'You successfully logged out'})
     unset_jwt_cookies(response)
     return response
@@ -189,112 +179,102 @@ api.add_resource(HelloApiHandler, '/flask/hello')
 
 ## ADMIN ACTIONS
 @app.route('/create_user', methods=['POST'])
+@jwt_required()
 def create_user():
-    if 'username' in session:
-        _json = request.json
-        _fullname = _json['fullname']
-        _mail = _json['mail']
-        _passwd = _json['passwd']
-        _rol = _json['rol']
+    _json = request.json
+    _fullname = _json['fullname']
+    _mail = _json['mail']
+    _passwd = _json['passwd']
+    _rol = _json['rol']
 
-        hashed_passwd = generate_password_hash(_passwd)
+    hashed_passwd = generate_password_hash(_passwd)
 
-        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-        # Checar que no exista el usuario 
+    # Checar que no exista el usuario 
+    sql = "SELECT * FROM users WHERE Mail=%s"
+    sql_where = (_mail,)
+
+    cursor.execute(sql, sql_where)
+    row = cursor.fetchone()
+
+    if not row:
+        # Arreglar IDs
+        count_query = 'SELECT COUNT(*) FROM users;'
+        cursor.execute(count_query)
+        count = cursor.fetchone()[0]
+
+        updated_count = count + 1
+
+        id_query = 'ALTER SEQUENCE users_id_user_seq RESTART WITH %s'
+        cursor.execute(id_query, (updated_count,))
+
+        # Crear usuario
+        query = 'INSERT INTO users(fullname, mail, passwd, rol) VALUES (%s, %s, %s, %s)'
+
+        query_args = (_fullname, _mail, hashed_passwd, str(_rol),)
+
+        cursor.execute(query, query_args)
+
+        # commit the changes to the database
+        conn.commit()
+
         sql = "SELECT * FROM users WHERE Mail=%s"
         sql_where = (_mail,)
 
         cursor.execute(sql, sql_where)
         row = cursor.fetchone()
 
+        # close communication with the database
+        cursor.close()
+
         if not row:
-            # Arreglar IDs
-            count_query = 'SELECT COUNT(*) FROM users;'
-            cursor.execute(count_query)
-            count = cursor.fetchone()[0]
-
-            updated_count = count + 1
-
-            id_query = 'ALTER SEQUENCE users_id_user_seq RESTART WITH %s'
-            cursor.execute(id_query, (updated_count,))
-
-            # Crear usuario
-            query = 'INSERT INTO users(fullname, mail, passwd, rol) VALUES (%s, %s, %s, %s)'
-
-            query_args = (_fullname, _mail, hashed_passwd, str(_rol),)
-
-            cursor.execute(query, query_args)
-
-            # commit the changes to the database
-            conn.commit()
-
-            sql = "SELECT * FROM users WHERE Mail=%s"
-            sql_where = (_mail,)
-
-            cursor.execute(sql, sql_where)
-            row = cursor.fetchone()
-
-            # close communication with the database
-            cursor.close()
-
-            if not row:
-                resp = jsonify({'message' : 'Bad Request - could not create user'})
-                resp.status_code = 400
-                return resp
-                
-            else:
-                return jsonify ({'message' : 'User successfully created',
-                            'user' : {
-                                "Id_user": row[0],
-                                "FullName": row[1],
-                                "Mail": row[2],
-                                "Rol": row[4]
-                            }
-                        })
-
-        else:
-            resp = jsonify({'message' : 'User already exists'})
+            resp = jsonify({'message' : 'Bad Request - could not create user'})
             resp.status_code = 400
             return resp
+            
+        else:
+            return jsonify ({'message' : 'User successfully created',
+                        'user' : {
+                            "Id_user": row[0],
+                            "FullName": row[1],
+                            "Mail": row[2],
+                            "Rol": row[4]
+                        }
+                    })
 
     else:
-        resp = jsonify({'message' : 'Unauthorized'})
-        resp.status_code = 401
+        resp = jsonify({'message' : 'User already exists'})
+        resp.status_code = 400
         return resp
 
 @app.route('/delete_user', methods=["DELETE"])
+@jwt_required()
 def delete_user():
-    if 'username' in session:
-        _json = request.json
-        _mail = _json['mail']
+    _json = request.json
+    _mail = _json['mail']
 
-        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-        # Checar que exista el usuario 
-        sql = "SELECT * FROM users WHERE Mail=%s"
-        sql_where = (_mail,)
+    # Checar que exista el usuario 
+    sql = "SELECT * FROM users WHERE Mail=%s"
+    sql_where = (_mail,)
 
-        cursor.execute(sql, sql_where)
-        row = cursor.fetchone()
+    cursor.execute(sql, sql_where)
+    row = cursor.fetchone()
 
-        if row:
-            query = "DELETE FROM users WHERE Mail=%s"
-            args = (_mail,)
+    if row:
+        query = "DELETE FROM users WHERE Mail=%s"
+        args = (_mail,)
 
-            cursor.execute(query, args)
-            conn.commit()
+        cursor.execute(query, args)
+        conn.commit()
 
-            return jsonify ({'message' : 'User successfully deleted'})
-
-        else:
-            resp = jsonify({'message' : 'Bad Request - could not find user'})
-            resp.status_code = 400
-            return resp
+        return jsonify ({'message' : 'User successfully deleted'})
 
     else:
-        resp = jsonify({'message' : 'Unauthorized'})
-        resp.status_code = 401
+        resp = jsonify({'message' : 'Bad Request - could not find user'})
+        resp.status_code = 400
         return resp
 
 ####################Test tabla DB expeses type
