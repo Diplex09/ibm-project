2fa883b54b514bcd5f9a097c4f2b0f045a4e882c
Sreddx <rssebas08@outlook.com>
2022-05-26 16:32:57 -0500

    Created class for Employee and ICA

---
+++ LERT/backend/DB_Connections/dbEmployee.py
@@ -0,0 +1,67 @@
+from ast import Import
+import json
+import sys
+from unicodedata import numeric
+import psycopg2
+import psycopg2.extras
+
+import sqlalchemy
+from sqlalchemy import *
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Session
+
+from flask import Flask, jsonify, request, session, send_from_directory
+
+from flask_cors import CORS
+
+from backend.DB_Connections.DBManager import DBManager
+
+Base = declarative_base()
+db = DBManager.getInstance() 
+
+
+class Employee(Base):
+    __tablename__ = "employee"
+    id_employee = Column(Integer, primary_key=True)
+    employee_name = Column(String(150))
+    employee_lastname = Column(String(150))
+    mail = Column(String(150))
+    country_origin = Column(String(150))
+    id_ica = Column(Integer, ForeignKey("ica.id_ica"))
+    country_residence = Column(String(150))
+    id_type = Column(Integer, ForeignKey("type.id_type"))
+    band = Column(Integer)
+    squad = Column(String(150))
+    start_date = Column(Date)
+    end_date = Column(Date)
+
+    def __init__(self, idEmp,name,lastName, mail, countryOrigin,idIcaTable,countryResidence,id_typeTable,band,squad,start_date,end_date):
+        self.id_employee = idEmp
+        self.employee_name = name
+        self.employee_lastname = lastName
+        self.mail = mail
+        self.country_origin = countryOrigin
+        self.id_ica = idIcaTable
+        self.country_residence = countryResidence
+        self.id_type = id_typeTable
+        self.band = band
+        self.squad = squad
+        self.start_date = start_date
+        self.end_date = end_date
+
+
+    def serialize(self):
+        return {
+            'employee_id': self.id_employee, 
+            'employeeName': self.employee_name,
+            'employeeLastName': self.employee_lastname,
+            'mail':  self.mail,
+            'countryOrigin': self.country_origin,
+            'ICA_ID': self.id_ica,
+            'countryResidence': self.country_residence,
+            'type_id': self.id_type,
+            'band': self.band,
+            'squad': self.squad,
+            'startDate': self.start_date,
+            'endDate': self.end_date
+        }
---
+++ LERT/backend/DB_Connections/dbIca.py
@@ -0,0 +1,111 @@
+from ast import Import
+import json
+import sys
+from tokenize import Double
+from unicodedata import numeric
+import psycopg2
+import psycopg2.extras
+
+import sqlalchemy
+from sqlalchemy import *
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Session
+
+from flask import Flask, jsonify, request, session, send_from_directory
+
+from flask_cors import CORS
+
+from backend.DB_Connections.DBManager import DBManager
+
+Base = declarative_base()
+db = DBManager.getInstance() 
+
+class ICA(Base):
+    __tablename__ = "ica"
+    id_ica = Column(Integer, primary_key=True)
+    ica_code = Column(String(120))
+    ica_core = Column(String(120))
+    year = Column(String(120))
+    id_planning = Column(String(50))
+    ica_owner = Column(String(150))
+    budget = Column(Integer)
+    country = Column(String(120))
+    status = Column(String(200))
+    depto = Column(String(50))
+    frequency_bill = Column(String(50))
+    cc = Column(String(50))
+    city_name_req = Column(String(100))
+    division = Column(String(50))
+    major = Column(String(50))
+    minor = Column(String(50))
+    leru - Column(String(50))
+    description = Column(String(200))
+    id_type = Column(Integer, ForeignKey("type.id_type"))
+    nec = Column(Integer)
+    total_plus_taxes = Column(Float)
+    start_Date = Column(Date)
+    end_date = Column(Date)
+    cty_name_perf = Column(String(50))
+    R_Cty_Perf = Column(String(100))
+    total_billing = Column(Float)
+
+    def __init__(self,idIca,icaCode,icaCore,year,idPlanning,icaOwner,budget,country,status,depto,frequencyBill,cc,cityNameReq,division,major,
+    minor,leru,description,idType,nec,totalPlusTax,startDate,endDate,ctyNamePerf,rCtyPerf,totalBilling):
+        self.id_ica = idIca
+        self.ica_code = icaCode
+        self.ica_core = icaCore
+        self.year = year
+        self.id_planning = idPlanning
+        self.ica_owner = icaOwner
+        self.budget = budget
+        self.country = country
+        self.status = status
+        self.depto = depto
+        self.frequency_bill = frequencyBill
+        self.cc = cc
+        self.city_name_req = cityNameReq
+        self.division = division
+        self.major = major
+        self.minor = minor
+        self.leru = leru
+        self.description = description
+        self.id_type = idType
+        self.nec = nec
+        self.total_plus_taxes = totalPlusTax
+        self.start_Date = startDate
+        self.end_date = endDate
+        self.cty_name_perf= ctyNamePerf
+        self.R_Cty_Perf = rCtyPerf
+        self.total_billing = totalBilling
+
+    def serialize(self):
+        return {
+            'id_ica': self.id_ica, 
+            'ica_code': self.ica_code,
+            'ica_core': self.ica_core,
+            'year': self.year,
+            'id_planning': self.id_planning,
+            'ica_owner': self.ica_owner,
+            'budget': self.budget,
+            'country': self.country,
+            'status': self.status,
+            'depto': self.depto ,
+            'frequency_bill': self.frequency_bill,
+            'cc': self.cc,
+            'city_name_req': self.city_name_req, 
+            'division': self.division,
+            'major': self.major,
+            'minor': self.minor,
+            'leru': self.leru,
+            'description': self.description,
+            'id_type': self.id_type,
+            'nec': self.nec,
+            'total_plus_taxes': self.total_plus_taxes,
+            'start_Date': self.start_Date,
+            'end_date': self.end_date ,
+            'cty_name_perf': self.cty_name_perf,
+            'R_Cty_Perf': self.R_Cty_Perf,
+            'total_billing': self.total_billing
+        }
+        
+        
