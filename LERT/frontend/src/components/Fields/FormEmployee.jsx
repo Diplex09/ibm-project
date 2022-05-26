c27a947f7299766f8b464045864a8e2e638a247d
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2022-05-26 12:45:14 -0500

    Update FormEmployee.jsx

--- LERT/frontend/src/components/Fields/FormEmployee.jsx
+++ LERT/frontend/src/components/Fields/FormEmployee.jsx
@@ -8,18 +8,28 @@ import {
     Paper,
     Modal,
     TextField,
+    InputAdornment,
+    Button,
 } from "@mui/material";
 import { DeleteOutlined } from "@mui/icons-material";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { useState, useEffect, Fragment } from "react";
 
+import { EmailOutlined, ArrowForwardOutlined } from "@mui/icons-material";
+
 export const FormEmployee = ({ closeModal }) => {
+    const [dateStart, setDateStart] = useState(new Date());
+    const [dateFinish, setDateFinish] = useState(new Date());
+
     return (
         <>
             <Modal
                 sx={{
                     marginLeft: "auto",
                     marginRight: "auto",
-                    top: "10rem",
+                    top: "5rem",
                     justifyContent: "center",
                     width: 700,
                 }}
@@ -30,7 +40,11 @@ export const FormEmployee = ({ closeModal }) => {
                 <Paper>
                     <Box>
                         <Typography
-                            sx={{ paddingLeft: 5, paddingTop: 3 }}
+                            sx={{
+                                paddingLeft: 5,
+                                paddingTop: 3,
+                                paddingBottom: 2,
+                            }}
                             variant="h5"
                             component="h2"
                         >
@@ -110,23 +124,89 @@ export const FormEmployee = ({ closeModal }) => {
                                     defaultValue="Hello World"
                                 />
 
-                                <TextField
-                                    required
-                                    size="small"
-                                    label="Start Date"
-                                    defaultValue="Hello World"
-                                />
+                                <LocalizationProvider
+                                    marginRight="50rem"
+                                    dateAdapter={AdapterDateFns}
+                                >
+                                    <DatePicker
+                                        renderInput={(params) => (
+                                            <TextField
+                                                sx={{ width: "14rem" }}
+                                                {...params}
+                                            />
+                                        )}
+                                        value={dateStart}
+                                        onChange={(date) => {
+                                            const d = new Date(date);
+                                            console.log(d);
+                                            setDateStart(d);
+                                        }}
+                                    />
+                                </LocalizationProvider>
                             </ListItem>
                             <ListItem>
-                                <TextField
-                                    required
-                                    size="small"
-                                    label="Finish Date"
-                                    defaultValue="Hello World"
-                                />
+                                <LocalizationProvider
+                                    marginRight="50rem"
+                                    dateAdapter={AdapterDateFns}
+                                >
+                                    <DatePicker
+                                        renderInput={(params) => (
+                                            <TextField
+                                                sx={{ width: "14rem" }}
+                                                {...params}
+                                            />
+                                        )}
+                                        value={dateFinish}
+                                        onChange={(date) => {
+                                            const dF = new Date(date);
+                                            console.log(dF);
+                                            setDateFinish(dF);
+                                        }}
+                                    />
+                                </LocalizationProvider>
+                            </ListItem>
+
+                            <ListItem sx={{ paddingTop: 5 }}>
+                                <Button
+                                    variant="contained"
+                                    sx={{
+                                        marginRight: 6,
+                                        textTransform: "none",
+                                        borderRadius: "4px",
+                                        width: "14rem",
+                                        height: "40px",
+                                        fontSize: "14px",
+                                        fontWeight: "400",
+                                        bgcolor: "#0062ff",
+                                        ":hover": {
+                                            bgcolor: "#0255DA",
+                                        },
+                                    }}
+                                >
+                                    Cancel
+                                </Button>
+                                <Button
+                                    variant="contained"
+                                    sx={{
+                                        marginRight: 6,
+                                        textTransform: "none",
+                                        borderRadius: "4px",
+                                        width: "14rem",
+                                        height: "40px",
+                                        fontSize: "14px",
+                                        fontWeight: "400",
+                                        bgcolor: "#0062ff",
+                                        ":hover": {
+                                            bgcolor: "#0255DA",
+                                        },
+                                    }}
+                                >
+                                    Modify
+                                </Button>
                             </ListItem>
                         </List>
                     </Box>
+
                     <IconButton onClick={() => closeModal(false)}>
                         <DeleteOutlined />
                     </IconButton>
