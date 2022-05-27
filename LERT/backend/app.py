032936aa56a535eace4dd31273d496153eafc0d2
Manuel Cota <56351434+mannynacc@users.noreply.github.com>
2022-05-27 11:07:01 -0600

    Moved app and config files to backend folder

--- LERT/backend/app.py
+++ LERT/backend/app.py
@@ -14,9 +14,10 @@ import psycopg2.extras
 from flask_cors import CORS #comment this on deployment
 from HelloApiHandler import HelloApiHandler
 from login import login
-from DB_Connections.dbInfo import getExpensesTypes, postExpenseType
-from DB_Connections.dbtypes import getTypes, postType
-
+from DB_Connections.dbUsers import all_users, edit_user, create_user, delete_user
+from DB_Connections.dbExpenseType import deleteExpenseType, getExpensesTypes, postExpenseType
+from DB_Connections.dbtypes import getTypes, postType, deleteType, updateType
+from DB_Connections.dbHours import getHours, postHours, deleteHour
 
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_jwt_extended import get_jwt, set_access_cookies, unset_jwt_cookies
@@ -32,7 +33,7 @@ from flask_jwt_extended import get_jwt, set_access_cookies, unset_jwt_cookies
 
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-CORS(app) #comment this on deployment
+CORS(app, support_credentials=True) #comment this on deployment
 api = Api(app)
 
 
@@ -114,13 +115,15 @@ def login():
         email = row[2]
         password = row[3]
         if row:
-            print(password)
             if check_password_hash(password, _password):
                 session['username'] = email
+                session['rol'] = row[4]
                 cursor.close()
                 response = jsonify({ 'msg': 'Login Successful', 'user' : {
-                        "uid": row[0],
-                        "fullName": row[1],
+                        "id_user": row[0],
+                        "fullname": row[1],
+                        "mail":
+                        row[2],
                         "rol": row[4]
                     }})
                 access_token = create_access_token(identity=row[0])
@@ -147,235 +150,47 @@ def logout():
     return response
 
 # Only for test
-@app.route("/protected")
+@app.route("/check")
 @jwt_required()
 def protected():
-    return jsonify({'msg' : 'Test Completed Correctly'})
+    uid = get_jwt_identity()
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    sql = "SELECT * FROM users WHERE id_user=%s"
+    sql_where = (uid,)
+
+    cursor.execute(sql, sql_where)
+    row = cursor.fetchone()
+    return jsonify({'msg' : 'Valid token', 'user' : {
+                        "uid": row[0],
+                        "fullName": row[1],
+                        "rol": row[4]
+                    }})
 
 api.add_resource(HelloApiHandler, '/flask/hello')
 
-# api.add_resource(login, '/login')
-
 ## ADMIN ACTIONS
-@app.route('/all_users', methods=['GET'])
-def all_users():
-    if 'username' in session:
-        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-        query = 'SELECT * FROM users'
-
-        cursor.execute(query)
-        people = cursor.fetchall()
-        cursor.close()
-
-        all_people = []
-
-        for person in people:
-            all_people.append(
-                {
-                    "Id_user": person[0],
-                    "FullName": person[1],
-                    "Mail": person[2],
-                    "Rol": person[4]
-                }
-            )
-
-        return jsonify({'people': all_people})
-
-    else:
-        resp = jsonify({'message' : 'Unauthorized'})
-        resp.status_code = 401
-        return resp
-
-
-@app.route('/create_user', methods=['POST'])
-def create_user():
-    if 'username' in session:
-        _json = request.json
-        _fullname = _json['fullname']
-        _mail = _json['mail']
-        _passwd = _json['passwd']
-        _rol = _json['rol']
-
-        hashed_passwd = generate_password_hash(_passwd)
-
-        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-        # Checar que no exista el usuario 
-        sql = "SELECT * FROM users WHERE Mail=%s"
-        sql_where = (_mail,)
-
-        cursor.execute(sql, sql_where)
-        row = cursor.fetchone()
-
-        if not row:
-            # Arreglar IDs
-            count_query = 'SELECT COUNT(*) FROM users;'
-            cursor.execute(count_query)
-            count = cursor.fetchone()[0]
-
-            updated_count = count + 1
-
-            id_query = 'ALTER SEQUENCE users_id_user_seq RESTART WITH %s'
-            cursor.execute(id_query, (updated_count,))
-
-            # Crear usuario
-            query = 'INSERT INTO users(fullname, mail, passwd, rol) VALUES (%s, %s, %s, %s)'
-
-            query_args = (_fullname, _mail, hashed_passwd, str(_rol),)
-
-            cursor.execute(query, query_args)
-
-            # commit the changes to the database
-            conn.commit()
-
-            sql = "SELECT * FROM users WHERE Mail=%s"
-            sql_where = (_mail,)
-
-            cursor.execute(sql, sql_where)
-            row = cursor.fetchone()
-
-            # close communication with the database
-            cursor.close()
-
-            if not row:
-                resp = jsonify({'message' : 'Bad Request - could not create user'})
-                resp.status_code = 400
-                return resp
-                
-            else:
-                return jsonify ({'message' : 'User successfully created',
-                            'user' : {
-                                "Id_user": row[0],
-                                "FullName": row[1],
-                                "Mail": row[2],
-                                "Rol": row[4]
-                            }
-                        })
-
-        else:
-            resp = jsonify({'message' : 'User already exists'})
-            resp.status_code = 400
-            return resp
-
-    else:
-        resp = jsonify({'message' : 'Unauthorized'})
-        resp.status_code = 401
-        return resp
-
-@app.route('/edit_user', methods=["PUT"])
-def edit_user():
-    if 'username' in session:
-        _json = request.json
-        _id = _json['id_user']
-
-        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-        # Checar que exista el usuario 
-        sql = "SELECT * FROM users WHERE id_user=%s"
-        sql_where = (str(_id,))
-
-        cursor.execute(sql, sql_where)
-        row = cursor.fetchone()
-
-        if row:
-            for col, val in _json.items():
-                print(col, val)
-                if col == "id_user":
-                    continue
-                elif col == "fullname":
-                    query = 'UPDATE users SET fullname = %s WHERE id_user = %s'
-
-                elif col == "mail":
-                    query = 'UPDATE users SET mail = %s WHERE id_user = %s'
-
-                elif col == "rol":
-                    query = 'UPDATE users SET rol = %s WHERE id_user = %s'
-
-                else:
-                    resp = jsonify({'message' : 'Bad Request - field does not match columns in DB'})
-                    resp.status_code = 400
-                    return resp
-                
-
-                query_args = (str(val), str(_id))
-                cursor.execute(query, query_args)
-
-                # commit the changes to the database
-                conn.commit()
-            
-            sql = "SELECT * FROM users WHERE id_user=%s"
-            sql_where = (str(_id,))
-
-            cursor.execute(sql, sql_where)
-            row = cursor.fetchone()
-
-            # close communication with the database
-            cursor.close()
-
-            return jsonify ({'message' : 'User info updated successfully',
-                            'user' : {
-                                "Id_user": row[0],
-                                "FullName": row[1],
-                                "Mail": row[2],
-                                "Rol": row[4]
-                            }
-                        })
-
-        else:
-            resp = jsonify({'message' : 'User does not exist'})
-            resp.status_code = 404
-            return resp
-
-    else:
-        resp = jsonify({'message' : 'Unauthorized'})
-        resp.status_code = 401
-        return resp
-
-@app.route('/delete_user', methods=["DELETE"])
-def delete_user():
-    if 'username' in session:
-        _json = request.json
-        _mail = _json['mail']
-
-        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-        # Checar que exista el usuario 
-        sql = "SELECT * FROM users WHERE Mail=%s"
-        sql_where = (_mail,)
-
-        cursor.execute(sql, sql_where)
-        row = cursor.fetchone()
-
-        if row:
-            query = "DELETE FROM users WHERE Mail=%s"
-            args = (_mail,)
-
-            cursor.execute(query, args)
-            conn.commit()
-
-            return jsonify ({'message' : 'User successfully deleted'})
-
-        else:
-            resp = jsonify({'message' : 'Bad Request - could not find user'})
-            resp.status_code = 400
-            return resp
-
-    else:
-        resp = jsonify({'message' : 'Unauthorized'})
-        resp.status_code = 401
-        return resp
 
 ####################Test tabla DB expeses type
+
+#Metodos users
+app.add_url_rule("/all_users", view_func=all_users, methods=['GET'])
+app.add_url_rule("/create_user", view_func=create_user, methods=['POST'])
+app.add_url_rule("/edit_user", view_func=edit_user, methods=['PUT'])
+app.add_url_rule("/delete_user", view_func=delete_user, methods=['DELETE'])
 
 #Metodos expensesTypes
 app.add_url_rule("/expensesTypes", view_func=getExpensesTypes, methods=['GET'])
 app.add_url_rule("/newExpenseType", view_func=postExpenseType, methods=['POST'])
+app.add_url_rule("/delExpenseType/<string:name>", view_func=deleteExpenseType, methods=['DELETE'])
 
 #Metodos types
 app.add_url_rule("/getTypes", view_func=getTypes, methods=['GET'])
 app.add_url_rule("/newPostType", view_func=postType, methods=['POST'])
+app.add_url_rule("/deleteTypes/<int:id>", view_func=deleteType, methods=['DELETE'])
+app.add_url_rule("/updateTypes/<int:id>", view_func=updateType, methods=['PUT'])
 
-
-
-
+#Metodos extraHours
+app.add_url_rule("/getHours", view_func=getHours, methods=['GET'])
+app.add_url_rule("/newPostHour", view_func=postHours, methods=['POST'])
+app.add_url_rule("/deleteHours/<int:id>", view_func=deleteHour, methods=['DELETE'])
