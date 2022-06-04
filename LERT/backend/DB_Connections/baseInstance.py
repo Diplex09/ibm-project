91ae4469285934c529e014bd48a271a4c7e059fa
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2022-06-04 18:36:39 -0500

    fix minor error

--- LERT/backend/DB_Connections/baseInstance.py
+++ LERT/backend/DB_Connections/baseInstance.py
@@ -4,7 +4,4 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import Table, Text
 
-from backend.DB_Connections.DBManager import DBManager
-db = DBManager.getInstance() 
-
 Base = declarative_base()
