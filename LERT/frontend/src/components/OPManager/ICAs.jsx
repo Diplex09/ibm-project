3af108b18659332392f9d9ac5de0954257239524
Sebastian Rojas Salazar <RSsebas08@outlook.com>
2022-06-10 11:45:53 -0500

    Removed comment so get ICA is called

--- LERT/frontend/src/components/OPManager/ICAs.jsx
+++ LERT/frontend/src/components/OPManager/ICAs.jsx
@@ -13,9 +13,9 @@ import { deleteIca, updateIca } from "../../actions/OP Manager/icas.js";
 export const Icas = () => {
     const [typeData, setTypeData] = useState([]);
 
-    // useEffect(() => {
-    //     fetchData();
-    // }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     const fetchData = () => {
         axios({
