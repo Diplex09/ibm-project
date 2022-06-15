6af13a628c2654d5cb9a18749fd6dfbc1e5d7392
Alonso G <71342528+AlonsoMtzG@users.noreply.github.com>
2022-06-14 23:37:25 -0500

    startLogin test

--- LERT/frontend/src/tests/hooks/useAuthStore.test.js
+++ LERT/frontend/src/tests/hooks/useAuthStore.test.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { Provider } from "react-redux";
 
-import { initialState } from "../fixtures/authStates";
+import { initialState, notAuthenticatedState } from "../fixtures/authStates";
 import { useAuthStore } from "../../hooks/useAuthStore";
 import { authSlice } from "../../reducers/authSlice";
+import { testUserCredentials } from "../fixtures/testUser";
 
 const getMockStore = (initialState) => {
     return configureStore({
@@ -37,6 +38,33 @@ describe("Test in auth actions", () => {
             startLogout: expect.any(Function),
             startChecking: expect.any(Function),
             checkingFinish: expect.any(Function),
+        });
+    });
+
+    test("startLogin should login correctly", async () => {
+        const mockStore = getMockStore({ ...notAuthenticatedState });
+
+        const { result } = renderHook(() => useAuthStore(), {
+            wrapper: ({ children }) => (
+                <Provider store={mockStore}>{children}</Provider>
+            ),
+        });
+
+        await act(async () => {
+            await result.current.startLogin(
+                testUserCredentials.email,
+                testUserCredentials.password
+            );
+        });
+
+        const { checking, uid, name, rol, rolName } = result.current;
+
+        expect({ checking, uid, name, rol, rolName }).toEqual({
+            checking: false,
+            uid: 4,
+            name: "Test OPManager",
+            rol: 2,
+            rolName: "Operation Manager",
         });
     });
 });
