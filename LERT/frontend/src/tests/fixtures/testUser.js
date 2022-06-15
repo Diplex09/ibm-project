66d2427a5892d4eba7054eee49c22ff81153e14a
Alonso G <71342528+AlonsoMtzG@users.noreply.github.com>
2022-06-14 22:55:23 -0500

    Completed authSlice tests

--- LERT/frontend/src/tests/fixtures/testUser.js
+++ LERT/frontend/src/tests/fixtures/testUser.js
@@ -1,6 +1,9 @@
 export const testUserCredentials = {
     email: "testopmanager@ibm.com",
     password: "password",
+};
+
+export const testUserInfo = {
     uid: 4,
     name: "Test OPManager",
     rol: 2,
--- LERT/frontend/src/tests/reducers/authSlice.test.js
+++ LERT/frontend/src/tests/reducers/authSlice.test.js
@@ -1,8 +1,25 @@
-import { authSlice } from "../../reducers/authSlice";
-import { initialState } from "../fixtures/authStates";
+import { authLogin, authLogout, authSlice } from "../../reducers/authSlice";
+import { authenticatedState, initialState } from "../fixtures/authStates";
+import { testUserInfo } from "../fixtures/testUser";
 
 describe("Tests in authSlice.js", () => {
     test("should return the initial state", () => {
         expect(authSlice.getInitialState()).toEqual(initialState);
+    });
+
+    test("should login", () => {
+        const state = authSlice.reducer(initialState, authLogin(testUserInfo));
+        expect(state).toEqual({
+            checking: false,
+            ...testUserInfo,
+        });
+    });
+
+    test("should logout", () => {
+        const state = authSlice.reducer(authenticatedState, authLogout());
+        expect(state).toEqual({
+            checking: false,
+            uid: undefined, // Shouldn't be in state
+        });
     });
 });
